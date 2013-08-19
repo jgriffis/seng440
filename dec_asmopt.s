f943e1ac7c1e7f6460ab176d42c485530fd280f3
Jared Griffis <jared.griffis@gmail.com>
2013-08-19 14:14:28 -0700

    Added and renamed assembly files

--- dec_asmopt.s
+++ dec_asmopt.s
@@ -156,126 +156,87 @@ main:
 	stmfd	sp!, {fp, lr}
 	add	fp, sp, #4
 	sub	sp, sp, #1056
-	sub	r3, fp, #536
+	sub	r1, fp, #536
 	ldr	r0, .L11
-	mov	r1, r3
 	bl	scanf
-	mov	r3, #0
-	str	r3, [fp, #-24]
+	mov	r4, #0
+    mov r5, #0
 	b	.L2
 .L3:
-	ldr	r0, [fp, #-24]
-	ldr	r3, [fp, #-24]
-	ldr	r2, .L11+4
 	sub	r1, fp, #4
-	add	r3, r1, r3
+	ldr	r2, .L11+4
+	add	r3, r1, r4
 	add	r3, r3, r2
-	ldrb	r3, [r3, #0]	@ zero_extendqisi2
+	ldrb	r3, [r3]	@ zero_extendqisi2
 	sub	r3, r3, #48
 	and	r1, r3, #255
 	ldr	r2, .L11+8
 	sub	ip, fp, #4
-	add	r3, ip, r0
+	add	r3, ip, r4
 	add	r2, r3, r2
-	mov	r3, r1
-	strb	r3, [r2, #0]
-	ldr	r3, [fp, #-24]
-	add	r3, r3, #1
-	str	r3, [fp, #-24]
+	strb	r1, [r2]
+	add	r4, r4, #1
 .L2:
-	ldr	r3, [fp, #-24]
 	ldr	r2, .L11+4
 	sub	r1, fp, #4
-	add	r3, r1, r3
+	add	r3, r1, r4
 	add	r3, r3, r2
-	ldrb	r3, [r3, #0]	@ zero_extendqisi2
+	ldrb	r3, [r3]	@ zero_extendqisi2
 	cmp	r3, #0
 	bne	.L3
-	mov	r3, #0
-	str	r3, [fp, #-20]
-	mov	r3, #0
-	str	r3, [fp, #-16]
-	mov	r3, #0
-	str	r3, [fp, #-12]
+	mov	r6, #0
+	str	r6, [fp, #-12]
 	b	.L4
 .L8:
-	ldr	r2, [fp, #-16]
-	ldr	r3, [fp, #-20]
-	add	r2, r2, r3
-	ldr	r3, [fp, #-24]
-	cmp	r2, r3
+	add	r2, r6, r5
+	cmp	r2, r4
 	blt	.L6
-	ldrb	r2, [fp, #-7]	@ zero_extendqisi2
-	ldr	r3, [fp, #-20]
-	rsb	r3, r3, #7
-	mov	r3, r2, asl r3
-	strb	r3, [fp, #-7]
+	rsb	r3, r5, #7
+	mov	r7, r7, asl r3
 	b	.L7
 .L6:
-	ldrb	r3, [fp, #-7]
-	mov	r3, r3, asl #1
-	strb	r3, [fp, #-7]
-	ldr	r2, [fp, #-16]
-	ldr	r3, [fp, #-20]
-	add	r3, r2, r3
+	mov	r7, r7, asl #1
+	add	r3, r6, r5
 	ldr	r2, .L11+8
-	sub	ip, fp, #4
 	add	r3, ip, r3
+	sub	ip, fp, #4
 	add	r3, r3, r2
-	ldrb	r2, [r3, #0]	@ zero_extendqisi2
-	ldrb	r3, [fp, #-7]
-	orr	r3, r2, r3
-	strb	r3, [fp, #-7]
-	ldr	r3, [fp, #-20]
-	add	r3, r3, #1
-	str	r3, [fp, #-20]
+	add	r5, r5, #1
+	ldrb	r2, [r3]	@ zero_extendqisi2
+	orr	r7, r2, r7
 .L5:
-	ldr	r3, [fp, #-20]
-	cmp	r3, #6
+	cmp	r5, #6
 	ble	.L8
 .L7:
-	ldrb	r3, [fp, #-7]	@ zero_extendqisi2
-	and	r2, r3, #127
+	and	r2, r7, #127
 	ldr	r3, .L11+12
 	ldrb	r3, [r3, r2]
 	strb	r3, [fp, #-6]
 	ldr	r0, [fp, #-12]
-	ldrb	r3, [fp, #-6]	@ zero_extendqisi2
 	mov	r3, r3, lsr #4
-	and	r3, r3, #255
 	add	r3, r3, #65
 	and	r1, r3, #255
 	ldr	r2, .L11+4
 	sub	ip, fp, #4
 	add	r3, ip, r0
 	add	r2, r3, r2
-	mov	r3, r1
-	strb	r3, [r2, #0]
-	ldr	r3, [fp, #-12]
-	add	r3, r3, #1
+	strb	r1, [r2]
+	add	r3, r0, #1
 	str	r3, [fp, #-12]
 	ldrb	r3, [fp, #-6]
 	and	r3, r3, #15
-	strb	r3, [fp, #-5]
-	ldrb	r2, [fp, #-5]	@ zero_extendqisi2
-	ldr	r3, [fp, #-16]
-	add	r3, r3, r2
-	str	r3, [fp, #-16]
-	ldrb	r3, [fp, #-5]	@ zero_extendqisi2
-	rsb	r3, r3, #7
-	str	r3, [fp, #-20]
+	add	r6, r6, r3
+	rsb	r5, r3, #7
 .L4:
-	ldr	r2, [fp, #-16]
-	ldr	r3, [fp, #-24]
-	cmp	r2, r3
+	cmp	r6, r4
 	blt	.L5
 	ldr	r3, [fp, #-12]
-	ldr	r2, .L11+4
 	sub	r1, fp, #4
+	ldr	r2, .L11+4
 	add	r3, r1, r3
 	add	r2, r3, r2
 	mov	r3, #0
-	strb	r3, [r2, #0]
+	strb	r3, [r2]
 	sub	r3, fp, #536
 	mov	r0, r3
 	bl	puts
